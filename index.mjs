bcc36384077037e2223029a40104f4b4a29aef71
Pandu Rendradjaja <3111845+prendradjaja@users.noreply.github.com>
2024-01-19 12:49:19 -0600

    Slope direction coloring

--- index.mjs
+++ index.mjs
@@ -5,7 +5,11 @@ import pngjs from "pngjs";
 const PNG = pngjs.PNG;
 
 const BLACK = 0;
-const WHITE = 255;
+const WHITE = 1;
+
+const maxSaturation = 0.4;
+const hue1 = 0;
+const hue2 = 0.666666;
 
 function parseArgs() {
   if (process.argv.length < 4) {
@@ -26,26 +30,53 @@ async function main() {
   const rasters = await image.readRasters();
   const { width, height, [0]: raster } = rasters;
 
-  // Make color scale
+  // Make lightness scale
   const f = makeLinearEquationFromPoints(
     [900, BLACK],
     [1, WHITE],
   );
   const elevationToBrightness = elevation =>
     elevation <= 0 // todo Check: Can it be exactly zero? (I think so: I think it's ints)
-    ? 50
-    : clamp(f(elevation), 0, 255);
+    ? 0.2
+    : clamp(f(elevation), 0, 1);
+
+  function getPartialColor(x, y) {
+    const xSlope = [2, 0, toElevation(x + 1, y) - toElevation(x - 1, y)];
+    const ySlope = [0, 2, toElevation(x, y + 1) - toElevation(x, y - 1)];
+    const normalVector = getCrossProduct(xSlope, ySlope);
+
+    // v points "down-slope"
+    const v = get2DComponent(normalVector);
+
+    // Except if [x, y] is on a flat surface
+    if (v[0] === 0 && v[1] === 0) {
+      return [0, 0];
+    }
+
+    // Use the direction of v to choose the color
+    const unitVector = getUnitVector2d(v);
+    if (unitVector[1] < 0) {
+      return [hue1, - maxSaturation * unitVector[1]];
+    } else {
+      return [hue2, maxSaturation * unitVector[1]];
+    }
+  }
+
+  const toIndex = (x, y) => (width * y + x) << 2;
+  const toElevation = (x, y) => raster[width * y + x];
 
   // Create and write image
   const outputImage = new PNG({ width, height });
-  for (let y = 0; y < height; y++) {
-    for (let x = 0; x < width; x++) {
-      const idx = (width * y + x) << 2;
-      const elevation = raster[x + y * width];
-      const brightness = elevationToBrightness(elevation);
-      outputImage.data[idx] = brightness;
-      outputImage.data[idx + 1] = brightness;
-      outputImage.data[idx + 2] = brightness;
+  for (let y = 1; y < height - 1; y++) {
+    for (let x = 1; x < width - 1; x++) {
+      const idx = toIndex(x, y);
+      const elevation = toElevation(x, y);
+      const l = elevationToBrightness(elevation);
+      const [h, s] = getPartialColor(x, y);
+      const [r, g, b] = hslToRgb(h, s, l);
+      outputImage.data[idx] = r;
+      outputImage.data[idx + 1] = g;
+      outputImage.data[idx + 2] = b;
       outputImage.data[idx + 3] = 255;
     }
   }
@@ -70,6 +101,69 @@ function makeLinearEquationFromPoints(p1, p2) {
   const m = (y2 - y1) / (x2 - x1); // slope
   const b = y1 - m * x1; // y-intercept
   return x => m * x + b;
+}
+
+/**
+ * Converts an HSL color value to RGB. Conversion formula
+ * adapted from https://en.wikipedia.org/wiki/HSL_color_space.
+ * Assumes h, s, and l are contained in the set [0, 1] and
+ * returns r, g, and b in the set [0, 255].
+ *
+ * @param   {number}  h       The hue
+ * @param   {number}  s       The saturation
+ * @param   {number}  l       The lightness
+ * @return  {Array}           The RGB representation
+ *
+ * From https://stackoverflow.com/a/9493060
+ */
+function hslToRgb(h, s, l) {
+  let r, g, b;
+
+  if (s === 0) {
+    r = g = b = l; // achromatic
+  } else {
+    const q = l < 0.5 ? l * (1 + s) : l + s - l * s;
+    const p = 2 * l - q;
+    r = hueToRgb(p, q, h + 1/3);
+    g = hueToRgb(p, q, h);
+    b = hueToRgb(p, q, h - 1/3);
+  }
+
+  return [
+    Math.round(r * 255),
+    Math.round(g * 255),
+    Math.round(b * 255)
+  ];
+}
+
+// From https://stackoverflow.com/a/9493060
+function hueToRgb(p, q, t) {
+  if (t < 0) t += 1;
+  if (t > 1) t -= 1;
+  if (t < 1/6) return p + (q - p) * 6 * t;
+  if (t < 1/2) return q;
+  if (t < 2/3) return p + (q - p) * (2/3 - t) * 6;
+  return p;
+}
+
+function get2DComponent(vector3d) {
+  return [vector3d[0], vector3d[1]];
+}
+
+// Get the unit vector in the same direction as the given 2D vector
+function getUnitVector2d(v) {
+  const magnitude = Math.sqrt(v[0]**2 + v[1]**2);
+  return [v[0] / magnitude, v[1] / magnitude];
+}
+
+function getCrossProduct(a, b) {
+  const [a1, a2, a3] = a;
+  const [b1, b2, b3] = b;
+  return [
+    a2*b3 - a3*b2,
+    a3*b1 - a1*b3,
+    a1*b2 - a2*b1,
+  ];
 }
 
 main();
